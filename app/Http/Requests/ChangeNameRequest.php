274a57d80fb64c1385ce6bba1be68a157659cedf
Zubco Mihail <zubco.mihail@meta.md>
2023-05-22 16:02:21 +0300

    settings

--- app/Http/Requests/ChangeNameRequest.php
+++ app/Http/Requests/ChangeNameRequest.php
@@ -2,11 +2,6 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Http\JsonResponse;
-
 /**
  * @OA\Schema(title="ChangeNameRequest", type="object")
  */
