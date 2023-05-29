0668ea67c63b6e36d335eb29525fb105ee493504
andrey Zhuk <zwer00@gmail.com>
2023-05-29 19:30:42 +0300

    fix Favorite button on the mobile

--- resources/views/partials/player-radio.blade.php
+++ resources/views/partials/player-radio.blade.php
@@ -36,7 +36,7 @@
             </div>
         </div>
         <div class="now-playing__actions">
-            <button class="btn btn_ico btn_ico-primary now-playing__fav-btn active" type="button" aria-label="{{ __('client.addToFavourites') }}">
+            <button class="btn btn_ico btn_ico-primary now-playing__fav-btn fav-station {{$favorited ? 'active' : ''}}" type="button" value="{{ $current['id'] }}" aria-label="{{ __('client.addToFavourites') }}">
                 <svg class="icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"></path>
                 </svg>
