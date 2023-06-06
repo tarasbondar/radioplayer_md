d687b8567ad15f8bfdfedd7678a0ae1e64ff3bdb
a-flood-32 <96569027+a-flood-32@users.noreply.github.com>
2023-06-06 15:16:29 +0300

    feature::player-swipe-open-not-finished

---
+++ resources/js/draggable-player.js
@@ -0,0 +1,7 @@
+export function draggablePlayer() {
+    const playerDragHandler = document.querySelector("#playerDragHandler");
+
+    // playerDragHandler.addEventListener("drag", (e) => {
+    //     console.log("darged");
+    // })
+}
