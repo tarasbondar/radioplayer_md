0032963a4cdffb6b1e23bb30543156736742292f
Zubco Mihail <zubco.mihail@meta.md>
2023-06-12 11:24:19 +0300

    roks artist and song api

--- app/Services/RadioApiService.php
+++ app/Services/RadioApiService.php
@@ -10,19 +10,23 @@ class RadioApiService
 {
     const API_HITFM = 1;
     const API_RADIORELAX = 2;
+    const API_ROKS = 3;
 
     const API_HITFM_URL = 'https://hitfm.md/data/current.json';
     const API_RADIORELAX_URL = 'https://www.radiorelax.md/getsong.php';
+    const API_ROKS_URL = 'http://217.26.165.218:47108/roks/broad.txt';
 
     private GuzzleClient $client;
 
     public static array $apis = [
         self::API_HITFM => self::API_HITFM_URL,
         self::API_RADIORELAX => self::API_RADIORELAX_URL,
+        self::API_ROKS => self::API_ROKS_URL,
     ];
     public static array $apiLabels = [
         self::API_HITFM => 'HitFM',
         self::API_RADIORELAX => 'RadioRelax',
+        self::API_ROKS => 'Roks',
     ];
 
     public function __construct(GuzzleClient $client)
@@ -32,9 +36,24 @@ class RadioApiService
 
 
     public function getStationInfo(RadioStation $station){
+        $request = null;
         $api = $station->api_id;
         $url = ($api) ? self::$apis[$api] : null;
-        $request = null;
+
+        $artist = '&nbsp;';
+        $song = $station->description;
+        $status = 'error';
+
+        $cache_key = sprintf('radio_info_v1_%s', sha1(serialize([$api, $station->source_meta])));
+        $cacheDuration = 5;
+
+        if ($data = cache($cache_key)) {
+            cache([$cache_key => $data], now()->addSeconds($cacheDuration));
+
+            return $data;
+        }
+
+
         if($api == self::API_HITFM){
             $request = new Request(
                 'GET',
@@ -45,14 +64,11 @@ class RadioApiService
             $response = $this->client->send($request);
             $body = $response->getBody();
             $data = json_decode($body, true);
-            if (isset($data[$station->source_meta]['artist']) && isset($data[$station->source_meta]['song']))
-                return [
-                    'status' => 'success',
-                    'artist' => $data[$station->source_meta]['artist'],
-                    'song' => $data[$station->source_meta]['song'],
-                ];
-
-
+            if (isset($data[$station->source_meta]['artist']) && isset($data[$station->source_meta]['song'])){
+                $status = 'success';
+                $artist = $data[$station->source_meta]['artist'];
+                $song = $data[$station->source_meta]['song'];
+            }
         }
         elseif($api == self::API_RADIORELAX){
             $requestData = [
@@ -69,20 +85,52 @@ class RadioApiService
             $data = json_decode($body, true);
             if (isset($data['name'])) {
                 $tmp = explode(' - ', $data['name']);
-                return [
-                    'status' => 'success',
-                    'artist' => $tmp[1],
-                    'song' => $tmp[0],
-                ];
+                $status = 'success';
+                $artist = $tmp[1];
+                $song = $tmp[0];
             }
 
         }
+        elseif($api == self::API_ROKS){
+            $request = new Request(
+                'GET',
+                $url,
+                ['Content-Type' => 'application/json']
+            );
 
-        return [
-            'status' => 'error',
-            'artist' => '&nbsp;',
-            'song' => $station->description
+            $response = $this->client->send($request);
+            $body = $response->getBody();
+            $contents = $body->getContents();
+
+            $lines = explode("\n", $contents);
+            $string = $lines[0];
+            $firstZeroIndex = strpos($string, "00");
+
+            if ($firstZeroIndex !== false) {
+                $secondZeroIndex = strpos($string, "00", $firstZeroIndex + 1);
+                if ($secondZeroIndex !== false) {
+                    $artistSong = trim(substr($string, $secondZeroIndex + 2));
+
+                    $lastDashIndex = strrpos($artistSong, "-");
+
+                    if ($lastDashIndex !== false) {
+                        $status = 'succes';
+                        $artist = trim(substr($artistSong, 0, $lastDashIndex));
+                        $song = trim(substr($artistSong, $lastDashIndex + 1));
+
+                    }
+                }
+            }
+
+        }
+        $result = [
+            'status' => $status,
+            'artist' => $artist,
+            'song' => $song
         ];
+
+        cache([$cache_key => $result], now()->addSeconds($cacheDuration));
+        return $result;
     }
 
 }
